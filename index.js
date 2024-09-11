c635c9a2e141a108c80e5bd7454fd718cbbf87b4
Pawan Kumar Baggi <101386982+Pawan-jsp@users.noreply.github.com>
2024-09-11 12:26:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'content-type': 'text/html' })
+// app.js
+const express = require('express');
+const app = express();
 
-	if (req.url === '/') {
-		res.write('<h1>Node and Nginx on Docker is Working</h1>')
-		res.end()
-	} else {
-		res.write('<h1>404 Nout Found</h1>')
-		res.end()
-	}
-})
+const PORT = 3000;
 
-server.listen(process.env.PORT || 3000, () => console.log(`server running on ${server.address().port}`))
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
